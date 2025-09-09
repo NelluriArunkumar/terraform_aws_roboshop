fa91c3a3e25197217d6e0c465b9df40951b91a98
NArunkumar <nelluri.arunkumar@gmail.com>
2025-09-09 19:13:41 +0530

    Terraform Module for Roboshop Components

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"  
+}
+
+variable "environment" {
+    default = "dev" 
+}
+
+variable "zone_id" {
+    default = "Z0541638YFS78XZ9O3N4" 
+}
+
+variable "zone_name" {
+    default = "arunkumarnelluri.site" 
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+  
+}
